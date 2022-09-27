f307b690b1591fb2aabe847e1053791c53b7cf16
amal_a_y <amal.a.alyahya@gmail.com>
2022-09-27 11:17:45 +0300

    last update

--- src/lockedMeProject/handlingOption.java
+++ src/lockedMeProject/handlingOption.java
@@ -2,20 +2,25 @@ package lockedMeProject;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.InputMismatchException;
+import java.util.List;
 import java.util.Scanner;
+
 
 public class handlingOption {
 	
 	 Scanner sc= new Scanner(System.in);
-	 
+	 public final static String ROOT_DIRECTORY_PATH="./files/";
 	
 
 	public static void dispalyMainMenu()
 	{	
+		
    try {
 		String msgPrint=
-				"please enter your chois number :\n \n" +
+				"\n \n please enter your chois number :\n \n" +
 	             "1-Retrieving the file\n"+
 				 "2-Display File operations menu \n"+
 	             "3-Exit the application";
@@ -43,14 +48,14 @@ public class handlingOption {
 	            	  break;
 	            	  
 	            default :
-	            	System.out.println("PLEAS ENTER FAID OPTION NUMBER");
+	            	System.out.println("PLEAS ENTER VAID OPTION NUMBER");
 	            	
 	            	
 	              
 	              }} catch (InputMismatchException e)
    {
 	           	   
-                  System.out.println("PLEAS ENTER FAID OPTION  \n");
+                  System.out.println("PLEAS ENTER VAID OPTION  \n");
                   dispalyMainMenu();
 
 
@@ -60,32 +65,37 @@ public class handlingOption {
 	              
 	              }
 	              
-		
+	// Retrieve all files	
 	public static void retriveAllFiles() {
 		
-	File folder = new File("./files/");
+	File folder = new File(ROOT_DIRECTORY_PATH);
 	File[] listOfFiles = folder.listFiles();
-
+	if (listOfFiles != null && listOfFiles.length > 0) {
+    System.out.println("The file in directory : \n "+"---------------------------" );
 	for (int i = 0; i < listOfFiles.length; i++) {
 	  if (listOfFiles[i].isFile()) {
-	    System.out.println("File " + listOfFiles[i].getName());
+	    System.out.println("File :" + listOfFiles[i].getName());
 	  } else if (listOfFiles[i].isDirectory()) {
 	    System.out.println("Directory " + listOfFiles[i].getName());
 	  }
 	}
-	
+	}else{
+		System.out.println("---- Empty Directory ---");
+	}
 	dispalyMainMenu();	
 	}
+	
+	
 	
 	public static void dispalyMenu()
 	
 	{	
 		try{
-			String s= "1-Add file to the application \n"+
-			 "2-Search file from the application\n" + 
-             "3-Delete file from the application\n"+
+			String s= "1-Add file to the Directory \n"+
+			 "2-Search file from the in Directory\n" + 
+             "3-Delete file from the Directory\n"+
 			 "4-Return to main menu \n"+
-			 "5-Exsit file from the application \n";
+			 "5-Exit file from the application \n";
 	
 	  System.out.println(s);
 	   
@@ -103,14 +113,15 @@ public class handlingOption {
        	  
          case 2:
         	 System.out.println("please enter the file name are you want to search: \n");
-       	  dispalyMenu();
-       	  break;
+        	 fileName=sc.next();
+        	 findFile( fileName);
+       	     dispalyMenu();
+       	     break;
        	  
          case 3:
         	 System.out.println("please enter the file name to delete: \n");
         	 fileName=sc.next();
-        	 System.out.println(fileName);
-       	     deleteFile(fileName);
+        	 deleteFile(fileName);
        	 
        	  break;
        	  
@@ -141,11 +152,12 @@ public class handlingOption {
 	}
 
 
-	
+	//create new file
 	  public static void creatFile(String fileName) {
 		    try {
-		      File myObj = new File("./files/"+fileName+".txt");
-		      if (myObj.createNewFile()) {
+		      File myObj = new File(ROOT_DIRECTORY_PATH+fileName+".txt");
+		      if (!myObj.exists()) {
+		        myObj.createNewFile();
 		        System.out.println("File created: " + myObj.getName()+ "\n");
 		        dispalyMenu();
 		      } else {
@@ -160,14 +172,68 @@ public class handlingOption {
 		  }
 	  
 	  
-	  public static void deleteFile(String fileName) { 
-		    File myObj = new File(fileName+".txt"); 
-		    if (myObj.delete()) { 
-		      System.out.println("Deleted the file: " + myObj.getName() +"\n");
-		      dispalyMenu();
-		    } else {
-		      System.out.println("File not found.\n");
-		      dispalyMenu();
-		    } 
-		  }
-}
+	 // delete function
+	  public static void deleteFile(String name)
+	    {
+		  boolean isExist=false;
+		    try {
+		    	File folder = new File(ROOT_DIRECTORY_PATH);
+		    	File[] Files = folder.listFiles();
+			      for(File fileEntry : Files) {
+			    	  //System.out.println(fileEntry.getName());
+			    	  if(fileEntry.isFile()) {
+			    	  if (fileEntry.getName().equals(name+".txt")) {
+			    		  isExist=true;
+			    		  fileEntry.delete();
+			    		
+			    	  }
+			    	  
+			      }}
+			    	  if(isExist) {
+			    		  System.out.println("File is delete \n");
+			    		  dispalyMenu();
+			    	  }
+			    	  else{
+			    		  System.out.println("File not found or check the case sensitivity\n"); 
+			    		  dispalyMenu();
+			    		  
+			    	  }
+			    	  
+			    } catch (Exception e) {
+			      System.out.println("An error occurred. \n");
+			      e.printStackTrace();
+			      dispalyMenu();
+			    }
+			  }
+
+	// search function
+	  public static void findFile(String name)
+	    {
+		  boolean isExist=false;
+		    try {
+		    	File folder = new File(ROOT_DIRECTORY_PATH);
+		    	File[] Files = folder.listFiles();
+			      for(File fileEntry : Files) {
+			    	  if(fileEntry.isFile()) {
+			    	  if (fileEntry.getName().equals(name+".txt")) {
+			    		  isExist=true;
+			    	  }
+			    	  
+			      }}
+			    	  if(isExist) {
+			    		  System.out.println("File is existed \n");
+			    	  }
+			    	  else{
+			    		  System.out.println("File is not existed in the directory\n"); 
+			    		  
+			    	  }
+			    	  
+			    } catch (Exception e) {
+			      System.out.println("An error occurred. \n");
+			      e.printStackTrace();
+			      dispalyMenu();
+			    }
+			  }
+
+	}
+
--- src/lockedMeProject/main.java
+++ src/lockedMeProject/main.java
@@ -2,24 +2,13 @@ package lockedMeProject;
 
 public class main {
  public static void main(String[] args) {
-		/*
-		 * String msgPrint="------------------------------\n"+
-		 * " Wellcome LockedMe.com \n " +
-		 * "This application was developed by Amal Alyahya \n" +
-		 * "--------------------------------------\n"+
-		 * "please enter your chois number :\n \n" + "1-Retrieving the file\n"+
-		 * "2-Display File operations menu \n"+ "3-Exit the application";
-		 * 
-		 * String s= "1- add file to the application \n"+
-		 * "2-search file from the application\n" +
-		 * "3-delete file from the application\n"+ "4-exsit file from the application"; *
-		 * 
-		 */
+		
+	 handlingOption bo = new handlingOption();
 	 System.out.println("-----------------------------------\n \n"+
              " Wellcome LockedMe.com \n " +
              "This application developed by Amal Alyahya \n \n" +
              "--------------------------------------\n");
-		handlingOption.dispalyMainMenu();//	handlingOption.creatFile("abc");xx //	handlingOption.deleteFile("fileName");
+		bo.dispalyMainMenu();
 
 	}
  }
